671cadcc426f9a5eb45ae0c1862dffbb2bd2f4cb
wr786 <wr786@pku.edu.cn>
2021-02-22 20:36:41 +0800

    style: The basic frame

--- src/index.js
+++ src/index.js
@@ -20,14 +20,38 @@ class ImageBox extends React.Component {
 }
 
 class MainPage extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      folderRelPath: "/assets/images/",
+      imagePrefix: "XVmZGe0",
+      imageSuffix: ".png",
+      imageTotNum: 131,
+      imageCurIdx: 1,
+      splitChar: '-',
+    };
+  }
+
+  calculateSRC = () => {
+    const folderRelPath = this.state.folderRelPath;
+    const imagePrefix = this.state.imagePrefix;
+    const imageSuffix = this.state.imageSuffix;
+    const imageCurIdx = this.state.imageCurIdx;
+    const splitChar = this.state.splitChar;
+    let src = folderRelPath + imagePrefix + splitChar  + imageCurIdx.toString() + imageSuffix;
+    return src; 
+  };
+
   render() {
+    const src = this.calculateSRC();
+    // console.log(src);
     return (
         <Layout className="layout">
         <Header style={{ textAlign: 'center', padding: '0.5em' }}>
           <Title><div className="white-text">GIF Frames Player</div></Title>
         </Header>
         <Content style={{ padding: '0 50px' }}>
-          <div className="site-layout-content">Content</div>
+          <div className="site-layout-content"><ImageBox src={src} /></div>
         </Content>
         <Footer style={{ textAlign: 'center' }}>GIF Frames Player by <Link href="https://github.com/wr786" target="_blank">wr786</Link> </Footer>
       </Layout>
